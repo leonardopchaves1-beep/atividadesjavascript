7d3b80f1386578c9cff320d03722d8ccb9d4e4a0
LEONARDO PINTO CHAVES <leonardo.pchaves@senacsp.edu.br>
2026-02-25 09:52:42 -0300

    primeiro commit

---
+++ 01-atividade/script.js
@@ -0,0 +1,60 @@
+// pergunta 1
+ 
+/* Uma empreendedora vende bolo de pote por R$ 12,00 cada.
+O custo de produção por unidade é R$ 5,50.
+Se ela vendeu 30 unidades, calcule:
+• O lucro por unidade
+• O lucro total */
+ 
+ 
+const preco = 12.00;
+let custo = 5.50;
+let quantidade = 30;
+let lucrounidade = preco - custo;
+let lucrototal = lucrounidade * quantidade;
+ 
+console.log ("Lucro por unidade: R$ " + lucrounidade);
+console.log("Lucro total: R$ " + lucrototal)
+
+/*Exercício 2 – Confeitaria (Desconto)
+
+constantes
+*/
+
+
+const precoBolo = 150.00;
+const descontoPercentual = 0.10;
+
+//interpolação 
+
+let valorDesconto = precoBolo * descontoPercentual;
+let valorFinal = precoBolo - valorDesconto;
+
+console.log(`Valor do desconto: R$ ${valorDesconto}`)
+console.log(`Valor final do bolo: R$ ${valorFinal}`)
+
+//pergunta 3
+ 
+/*
+Uma estilista vendeu um vestido por R$ 800,00.
+Ela recebe 15% de comissão sobre a venda.
+Calcule o valor da comissão e exiba o resultado usando interpolação */
+ 
+ 
+const valorvestido = 800.00;
+ 
+const porcentagem = 15;
+let valorcomissao = (valorvestido * porcentagem) / 100;
+console.log (`O valor da comissão é R$ ${valorcomissao}`)
+
+//Exercício 4 – Psicólogo (Faturamento Mensal)
+
+//constantes
+
+const valorSessao = 120.00;
+const quatidadedeSessoes = 40;
+
+let faturamento = valorSessao * quatidadedeSessoes
+
+console.log(`Faturamento mensal: R$ ${faturamento}`)
+
